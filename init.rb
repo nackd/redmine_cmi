bc334d9519b275dd301b61bb1fe22280126e4872
Juanje Ojeda <jojeda@emergya.es>
2010-11-17 12:33:01 +0100

    Some i18n improvements

--- init.rb
+++ init.rb
@@ -12,9 +12,9 @@ require 'users_helper_patch'
 require 'project_patch'
 
 Redmine::Plugin.register :redmine_cmiplugin do
-  name 'Cuadro de Mando Integral'
+  name :plugin_name
   author 'Emergya Consultoría'
-  description ' Vistas resumenes globales y por proyectos'
+  description :plugin_description
   version '1.1'
 
   settings :default => { }
@@ -25,10 +25,10 @@ Redmine::Plugin.register :redmine_cmiplugin do
         permission :view_metrics, {:metrics => [:show]}
   end
 
-  menu :project_menu, :metrics, {:controller => 'metrics', :action => 'show' }, :caption => 'Métricas', :after => :settings, :param => :project_id
+  menu :project_menu, :metrics, {:controller => 'metrics', :action => 'show' }, :caption => :caption_metrics, :after => :settings, :param => :project_id
   menu :top_menu, :cmi, {:controller => 'management', :action => 'projects'}, :caption => 'CMI', :if => Proc.new { User.current.admin? }
-  menu :scoreboard_menu, :projects, {:controller => 'management', :action => 'projects' }, :caption => 'Proyectos'
-  menu :scoreboard_menu, :status, {:controller => 'management', :action => 'status' }, :caption => 'Estado'
-  menu :scoreboard_menu, :groups, {:controller => 'management', :action => 'groups' }, :caption => 'Grupos'
+  menu :scoreboard_menu, :projects, {:controller => 'management', :action => 'projects' }, :caption => :caption_projects
+  menu :scoreboard_menu, :status, {:controller => 'management', :action => 'status' }, :caption => :caption_status
+  menu :scoreboard_menu, :groups, {:controller => 'management', :action => 'groups' }, :caption => :caption_groups
   menu :admin_menu, :label_cost_history, {:controller => 'admin', :action => 'cost_history'}, :class => 'issue_statuses'
 end
