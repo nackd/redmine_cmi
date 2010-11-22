49948db7e15357032fad8860ea256fa4b3dbbf9e
Juanje Ojeda <jojeda@emergya.es>
2010-11-22 10:10:17 +0100

    fix: Added alias_method_chain to overwrite the display_main_menu? method.      The CMI menu wasn't displaying because of the check of that method.

--- lib/scoreboard_menu_helper_patch.rb
+++ lib/scoreboard_menu_helper_patch.rb
@@ -6,6 +6,7 @@ module MenuHelperPatch
 
     base.class_eval do
       alias_method_chain :render_main_menu, :score_menu
+      alias_method_chain :display_main_menu?, :score_menu
     end
   end
 
@@ -17,6 +18,14 @@ module MenuHelperPatch
         render_menu :scoreboard_menu
       else
         render_main_menu_without_score_menu project
+      end
+    end
+    def display_main_menu_with_score_menu?(project)
+      # Core defined data
+      if params[:controller] == 'management'
+        return true
+      else
+        return false
       end
     end
   end
