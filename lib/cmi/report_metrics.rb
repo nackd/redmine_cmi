238d878140a6a64f321a9057c55aee0fedb2b615
Jose M. Prieto <jmprieto@gmx.net>
2011-02-21 22:10:45 +0100

    metrics: refactor remaining effort

--- lib/cmi/report_metrics.rb
+++ lib/cmi/report_metrics.rb
@@ -13,6 +13,16 @@ module CMI
       @project.effort_done_by_role(role, @date)
     end
 
+    def effort_planned_by_role(role)
+      role_current_effort_planned_name = DEFAULT_VALUES['report_role_current_effort_planned_field'].gsub('{{role}}', role)
+      role_current_effort_planned = IssueCustomField.find_by_name(role_current_effort_planned_name)
+      @report.custom_value_for(role_current_effort_planned).value.to_f rescue 0.0
+    end
+
+    def effort_remaining_by_role(role)
+      effort_planned_by_role(role) - effort_done_by_role(role)
+    end
+
     private
 
     def method_missing(method, *args, &block)
