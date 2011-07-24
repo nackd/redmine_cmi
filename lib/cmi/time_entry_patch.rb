eaea5c5d2ac8dd2843ce598a42e7f24ab9d76348
Jose M. Prieto <jmprieto@gmx.net>
2011-07-24 13:36:40 +0200

    time_entry_patch: fix role and cost calculation

--- lib/cmi/time_entry_patch.rb
+++ lib/cmi/time_entry_patch.rb
@@ -12,7 +12,6 @@ module CMI
       base.class_eval do
         unloadable # Send unloadable so it will be reloaded in development
         before_save :update_role_and_cost
-        before_validation_on_create :initialize_role
       end
     end
 
@@ -20,15 +19,10 @@ module CMI
     end
 
     module InstanceMethods
-      def initialize_role
-        self.role = User.current.role
-      end
-
       def update_role_and_cost
-        @hash_cost_actual_year = (HistoryProfilesCost.find :all).group_by(&:year)[Date.today.year].group_by(&:profile)
-        user_role = self.role.nil? ? (!User.current.role.blank? ? User.current.role : "") : self.role
-        if attribute_present?("hours") and !user_role.blank?
-          self.role = User.current.role if self.role.nil? and !User.current.role.blank?
+        self.role = self.user.role
+        @hash_cost_actual_year = (HistoryProfilesCost.find :all).group_by(&:year)[self.tyear].group_by(&:profile)
+        if attribute_present?("hours") and self.role.present?
           self.cost = (self.hours.to_f * @hash_cost_actual_year["#{self.role}"].first.value.to_f)
         end
       end
