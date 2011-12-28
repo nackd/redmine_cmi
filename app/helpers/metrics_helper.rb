8f5c61e02ee9495ae1857c0916632b035976da34
Jose M. Prieto <jmprieto@gmx.net>
2011-12-28 21:58:56 +0100

    helpers: drop old code from MetricsHelper

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -1,20 +1,4 @@
 module MetricsHelper
-  def set_value(profile, report, denominator, numerator)
-    instance_variable_set("@value_#{profile.underscore}_#{report}", instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f > 0.0? (instance_variable_get("@metrics_#{report}")["#{numerator}"].to_f * 100 /instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f).round(2) : "0.0")
-  end
-
-  def set_cost_value(profile, report, denominator, numerator)
-    instance_variable_set("@cost_#{profile.underscore}_#{report}", instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f > 0.0? (instance_variable_get("@metrics_#{report}")["#{numerator}"].to_f * 100 /instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f).round(2) : "0.0")
-  end
-
-  def get_report_value(report, denominator, numerator=nil)
-    unless numerator.nil?
-      instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f > 0.0? (instance_variable_get("@metrics_#{report}")["#{numerator}"].to_f * 100 /instance_variable_get("@metrics_#{report}")["#{denominator}"].to_f).round(2) : 0.0
-    else
-      instance_variable_get("@metrics_#{report}")["#{denominator}"]
-    end
-  end
-
   def effort_done_graph(report, roles)
     total = report.effort_done
     percent = roles.collect { |role| total.zero? ? 0.0 : (report.effort_done_by_role(role) * 100 / total).round(2) }
