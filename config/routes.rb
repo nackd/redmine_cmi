679eccf60f5728e478f9be66eee24f8be71e76fd
Jose M. Prieto <jmprieto@gmx.net>
2012-01-22 21:58:41 +0100

    admin: fix costs history for redmine current master

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ ActionController::Routing::Routes.draw do |map|
                 :member => { :preview => :put, :new_journal => :post, :edit_journal => [:get, :post] }
   map.metrics '/projects/:project_id/metrics/:action', :controller => 'metrics'
   map.management '/management/:action', :controller => 'management'
+  map.cost_history '/admin/cost_history', :controller => 'admin', :action => 'cost_history'
 end
