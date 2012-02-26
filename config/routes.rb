0030344bb1595fc27a32dbef9d8799c95007e801
Jose M. Prieto <jmprieto@gmx.net>
2012-02-26 22:39:16 +0100

    fix checkpoint and expenditure preview

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :checkpoints,
                 :path_prefix => '/projects/:project_id/metrics',
-                :member => { :preview => :put, :new_journal => :post, :edit_journal => [:get, :post] }
+                :member => { :new_journal => :post, :edit_journal => [:get, :post] },
+                :collection => { :preview => :put }
   map.resources :expenditures,
                 :path_prefix => '/projects/:project_id/metrics',
-                :member => { :preview => :put, :new_journal => :post, :edit_journal => [:get, :post] }
+                :member => { :new_journal => :post, :edit_journal => [:get, :post] },
+                :collection => { :preview => :put }
   map.metrics '/projects/:project_id/metrics/:action', :controller => 'metrics'
   map.management '/management/:action', :controller => 'management'
   map.cost_history '/admin/cost_history', :controller => 'admin', :action => 'cost_history'
