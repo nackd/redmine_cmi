d9f6c299338fa7e67656e19f84a7a62f890a6da7
Jose M. Prieto <jmprieto@emergya.com>
2012-04-09 11:06:59 +0200

    defaults for missing custom values

--- lib/cmi/common.rb
+++ lib/cmi/common.rb
@@ -5,7 +5,18 @@ module CMI
       def get_customs(project, project_metrics)
         INITIAL_METRICS.each do |custom_field_name|
           id = ProjectCustomField.find_by_name(custom_field_name)
-          project_metrics[custom_field_name] = (project.custom_values.find_by_custom_field_id(id)).value unless id.nil?
+          begin
+            project_metrics[custom_field_name] = project.custom_value_for(id).value unless id.nil?
+          rescue NoMethodError
+            project_metrics[custom_field_name] = case id.field_format
+                                                 when 'float'
+                                                   0.0
+                                                 when 'int'
+                                                   0
+                                                 else
+                                                   raise
+                                                 end
+          end
         end
         return project_metrics
       end
@@ -15,7 +26,18 @@ module CMI
         project_metrics['Fecha del informe']=informe.start_date.to_s
         VARIANT_METRICS.each do |custom_field_name|
           id = IssueCustomField.find_by_name(custom_field_name)
-          project_metrics[custom_field_name] = (informe.custom_values.find_by_custom_field_id(id)).value unless id.nil?
+          begin
+            project_metrics[custom_field_name] = informe.custom_value_for(id).value unless id.nil?
+          rescue NoMethodError
+            project_metrics[custom_field_name] = case id.field_format
+                                                 when 'float'
+                                                   0.0
+                                                 when 'int'
+                                                   0
+                                                 else
+                                                   raise
+                                                 end
+          end
         end
         return [project_metrics, @informe]
       end
